374a0be7e760940a4fe57251d0b58367048ffc24
Gosha Guryanov <billclintooth@gmail.com>
2015-03-16 04:56:45 -0400

    Extract spawn_phantom

--- node-phantom.js
+++ node-phantom.js
@@ -1,15 +1,14 @@
 //Released to the public domain.
-var http         = require('http')
-,	fs           = require('fs')
-,	path         = require('path')
-,	socketio     = require('socket.io')
-,	child        = require('child_process')
-,	test         = require('assert')
-,	PageProxy    = require('./pageproxy')
-,	PhantomProxy = require('./phantomproxy')
-,	Responder    = require('./responder')
-,	stub         = fs.readFileSync(path.join(__dirname, "stub.html"))
-,	debug        = console.log
+var http          = require('http')
+,	fs            = require('fs')
+,	path          = require('path')
+,	socketio      = require('socket.io')
+,	PageProxy     = require('./pageproxy')
+,	PhantomProxy  = require('./phantomproxy')
+,	Responder     = require('./responder')
+,	spawn_phantom = require('./spawn_phantom')
+,	stub          = fs.readFileSync(path.join(__dirname, "stub.html"))
+,	debug         = console.log
 ;
 
 var makeCallback = function (callback) {
@@ -28,132 +27,73 @@ var httpReqListener = function (req, res) {
 	res.end(stub);
 };
 
-var spawn_phantom = (function() {
-	var self = function (port, io, options, initCompletedCallback) {
-		self.port     = port;
-		self.io       = io;
-		self.opts     = options;
-		self.initCompletedCallback = initCompletedCallback;
-		self.spawn();
-		self.bindEvents();
-		self.initComplete();
-	};
-
-	self.spawn = function() {
-		var args = [];
-		for (var parm in self.opts.parameters) {
-			args.push('--' + parm + '=' + self.opts.parameters[parm]);
-		}
-		args = args.concat([__dirname + '/bridge.js', self.port]);
-		self.phantom_ps = child.spawn(self.opts.phantomPath, args);
-	};
-
-	self.hasErrors = false;
-
-	self.bindEvents = function() {
-		var phantom = self.phantom_ps;
-		phantom.stdout.on('data', self.stdout_data);
-		phantom.stderr.on('data', self.stderr_data);
-		phantom.on('error', self.phantom_error);
-		phantom.on('exit', self.phantom_error);
-	};
-
-	self.initComplete = function() {
-		process.nextTick(function() {
-			self.initCompletedCallback(self.hasErrors, self.phantom_ps);
-		});
-	};
-
-	self.prematureExitOn = function() {
-		// An exit event listener that is registered AFTER the phantomjs process
-		// is successfully created.
-		self.prematureExitHandler = function (code, signal) {
-			console.warn('phantom crash: code ' + code);
-			self.io.httpServer.close();
-		};
-		self.phantom_ps.on('exit', self.prematureExitHandler);
-	};
-	//an exit is no longer premature now
-	self.prematureExitOff = function() {
-		self.phantom_ps.removeListener('exit', self.prematureExitHandler);
-	};
-	self.stdout_data = function (data) {
-		console.log('phantom stdout: ' + data);
-	};
-	self.stderr_data = function (data) {
-		console.log('phantom stderr: ' + data);
-	};
-	self.phantom_error = function (data) {
-		self.hasErrors = true;
-	};
-
-  return self;
-})();
-
-
 
 module.exports = {
-	create: function (callback, options) {
-		if (options === undefined) options = {};
-		if (options.phantomPath === undefined) options.phantomPath = 'phantomjs';
-		if (options.parameters === undefined) options.parameters = {};
+	create: function (userCallback, userOptions) {
+		if (!userOptions) { userOptions = {} };
+		if (!userOptions.phantomPath) { userOptions.phantomPath = 'phantomjs' };
+		if (!userOptions.parameters)  { userOptions.parameters  = {} };
 
 		var server = http.createServer(httpReqListener);
 		server.listen(function () {
 			var port = server.address().port
 			,	io   = socketio.listen(server, { 'log level': 1});
-			spawn_phantom(port, io, options, function (err, phantom) {
-				if (err) {
-					debug('spawn_phantom.initCompletedCallback/err', err);
-					io.httpServer.close();
-					callback(true);
-					return;
-				}
-				var pages = {}
-				,	cmdid = 0
-				,	cmds  = {}
-				;
+			spawn_phantom({
+				options: userOptions,
+				port:    port,
+				io:      io,
+				spawnded: function (err, phantom) {
+					if (err) {
+						debug('spawn_phantom.initCompletedCallback/err', err);
+						io.httpServer.close();
+						userCallback(true);
+						return;
+					}
+					var pages = {}
+					,	cmdid = 0
+					,	cmds  = {}
+					;
 
-				function request(socket, args, callback) {
-					args.splice(1, 0, cmdid);
-					socket.emit('cmd', JSON.stringify(args));
-					cmds[cmdid] = {
-						cb: makeCallback(callback)
-					};
-					cmdid++;
-				}
+					function request(socket, args, callback) {
+						args.splice(1, 0, cmdid);
+						socket.emit('cmd', JSON.stringify(args));
+						cmds[cmdid] = {
+							cb: makeCallback(callback)
+						};
+						cmdid++;
+					}
 
-				io.sockets.on('connection', function (socket) {
-					socket.on('res', function (res) {
-						debug('socket.res\n', res);
-						new Responder({
-							response: res,
-							pages: pages,
-							cmds: cmds,
+					io.sockets.on('connection', function (socket) {
+						socket.on('res', function (res) {
+							debug('socket.res\n', res);
+							new Responder({
+								response: res,
+								pages: pages,
+								cmds: cmds,
+								socket: socket,
+								server: server,
+								request: request
+							}).dispatch();
+						});
+						socket.on('push', function (req) {
+							debug('socket.push\n', req);
+							var id       = req[0]
+							,	cmd      = req[1]
+							,	args     = unwrapArray(req[2])
+							,	callback = makeCallback(pages[id] ? pages[id][cmd] : undefined)
+							;
+							callback(args);
+						});
+						userCallback(null, new PhantomProxy({
 							socket: socket,
-							server: server,
-							request: request
-						}).dispatch();
+							request: request,
+							phantom: phantom,
+							spawn_phantom: spawn_phantom
+						}));
 					});
-					socket.on('push', function (req) {
-						debug('socket.push\n', req);
-						var id       = req[0]
-						,	cmd      = req[1]
-						,	args     = unwrapArray(req[2])
-						,	callback = makeCallback(pages[id] ? pages[id][cmd] : undefined)
-						;
-						callback(args);
-					});
-					var proxy = new PhantomProxy({
-						socket: socket,
-						request: request,
-						phantom: phantom,
-						spawn_phantom: spawn_phantom
-					});
-					callback(null, proxy);
-				});
 
-				spawn_phantom.prematureExitOn(); // not instance, but class
+					spawn_phantom.prematureExitOn(); // not instance, but class
+				}
 			});
 		});
 	}
